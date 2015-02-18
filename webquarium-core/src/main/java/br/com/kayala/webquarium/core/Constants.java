9de8eaa72de19eae0acd9ba4590fdfa329e59674
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-18 15:24:05 -0200

    Core constants class

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/Constants.java
@@ -0,0 +1,25 @@
+package br.com.kayala.webquarium.core;
+
+/**
+ * Core constants class
+ *
+ * @author kayala
+ */
+public class Constants {
+
+	/**
+	 * Database JNDI name
+	 */
+	public static final String DB_JNDI_NAME = "java:/jdbc/webquarium";
+
+	/**
+	 * JPA entity package to scan
+	 */
+	public static final String ENTITY_PACKAGE = "br.com.kayala.webquarium.core.model";
+
+	/**
+	 * Spring Data JPA repository package to scan
+	 */
+	public static final String REPOSITORY_PACKAGE = "br.com.kayala.webquarium.core.model.repository";
+
+}
