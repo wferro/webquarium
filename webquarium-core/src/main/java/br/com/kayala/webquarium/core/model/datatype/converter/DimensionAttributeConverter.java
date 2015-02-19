b0bad92d38bd03125eab95f3617cd1cbebf62fca
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:43:29 -0200

    jpa dimension datatype attribute converter

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/model/datatype/converter/DimensionAttributeConverter.java
@@ -0,0 +1,34 @@
+package br.com.kayala.webquarium.core.model.datatype.converter;
+
+import br.com.kayala.webquarium.core.model.datatype.Dimension;
+import java.math.BigDecimal;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * jpa dimension datatype attribute converter
+ *
+ * @author kayala
+ */
+@Converter(autoApply = true)
+public class DimensionAttributeConverter implements AttributeConverter<Dimension, BigDecimal> {
+
+	@Override
+	public BigDecimal convertToDatabaseColumn(Dimension x) {
+		if (x == null) {
+			return null;
+		} else {
+			return x.getRawValue();
+		}
+	}
+
+	@Override
+	public Dimension convertToEntityAttribute(BigDecimal y) {
+		if (y == null) {
+			return null;
+		} else {
+			return new Dimension(y);
+		}
+	}
+
+}
