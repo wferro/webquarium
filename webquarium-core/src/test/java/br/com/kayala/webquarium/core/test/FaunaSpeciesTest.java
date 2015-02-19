868887057d374f93304a43228d00787be9881e46
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:59:02 -0200

    fauna species unit test

---
+++ webquarium-core/src/test/java/br/com/kayala/webquarium/core/test/FaunaSpeciesTest.java
@@ -0,0 +1,109 @@
+package br.com.kayala.webquarium.core.test;
+
+import br.com.kayala.webquarium.core.model.datatype.TankPosition;
+import br.com.kayala.webquarium.core.model.entity.FaunaSpecies;
+import br.com.kayala.webquarium.core.model.entity.FaunaSpeciesCommonName;
+import br.com.kayala.webquarium.core.model.entity.Source;
+import br.com.kayala.webquarium.core.model.repository.FaunaSpeciesRepository;
+import java.math.BigDecimal;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Fauna species unit test
+ *
+ * @author kayala
+ */
+public class FaunaSpeciesTest extends BaseTest {
+
+	@Autowired
+	private FaunaSpeciesRepository repo;
+
+	private Long generatedId;
+
+	@Autowired
+	private Validator validator;
+
+	@Before
+	public void before() {
+		generatedId = createFaunaSpecies();
+	}
+
+	private Long createFaunaSpecies() {
+		FaunaSpecies faunaSpecies = new FaunaSpecies();
+
+		faunaSpecies.setAdultSize(BigDecimal.valueOf(5.5D));
+		faunaSpecies.setBehavior("Comportamento junit");
+		faunaSpecies.setCharacteristics("Caracteristicas junit");
+
+		faunaSpecies.getCommonNames().add(new FaunaSpeciesCommonName("Nomo popular 1 junit", faunaSpecies));
+		faunaSpecies.getCommonNames().add(new FaunaSpeciesCommonName("Nomo popular 2 junit", faunaSpecies));
+		faunaSpecies.getCommonNames().add(new FaunaSpeciesCommonName("Nomo popular 3 junit", faunaSpecies));
+		faunaSpecies.getCommonNames().add(new FaunaSpeciesCommonName("Nomo popular 4 junit", faunaSpecies));
+
+		faunaSpecies.setFamily("Familia junit");
+		faunaSpecies.setFood("Alimentação junit");
+		faunaSpecies.setGenus("Gênero junit");
+		faunaSpecies.setGhMax(7);
+		faunaSpecies.setGhMin(1);
+		faunaSpecies.setHabitat("Habitat natural junit");
+		faunaSpecies.setImagePath("/teste/imagem");
+		faunaSpecies.setKhMax(9);
+		faunaSpecies.setKhMin(1);
+		faunaSpecies.setKigdom("Reino junit");
+		faunaSpecies.setSpeciesOrder("Ordem junit");
+		faunaSpecies.setOtherInformation("Outras informações junit");
+		faunaSpecies.setPhMax(BigDecimal.valueOf(8.5D));
+		faunaSpecies.setPhMin(BigDecimal.valueOf(6.5D));
+		faunaSpecies.setPhylum("Filo junit");
+		faunaSpecies.setReproduction("Reprodução junit");
+		faunaSpecies.setSexualDimorphism("Dimorfismo sexual junit");
+
+		faunaSpecies.getSources().add(new Source("Source 1 junit", faunaSpecies));
+		faunaSpecies.getSources().add(new Source("Source 2 junit", faunaSpecies));
+		faunaSpecies.getSources().add(new Source("Source 3 junit", faunaSpecies));
+		faunaSpecies.getSources().add(new Source("Source 4 junit", faunaSpecies));
+
+		faunaSpecies.setSpecies("Espécie junit");
+		faunaSpecies.setSpeciesClass("Classe junit");
+		faunaSpecies.setSubfamily("Sub-família junit");
+		faunaSpecies.setTankPosition(TankPosition.TopMiddle);
+		faunaSpecies.setTemperatureMax(28);
+		faunaSpecies.setTemperatureMin(23);
+
+		return repo.save(faunaSpecies).getId();
+	}
+
+	@After
+	public void after() {
+		FaunaSpecies faunaSpecies = repo.findOne(generatedId);
+		if (faunaSpecies != null) {
+			repo.delete(faunaSpecies);
+		}
+	}
+
+	@Test
+	public void testCompareNumberValidator() {
+		FaunaSpecies faunaSpecies = repo.findOne(generatedId);
+		faunaSpecies.setPhMin(BigDecimal.TEN);
+		faunaSpecies.setPhMax(BigDecimal.ONE);
+		faunaSpecies.setGhMin(10);
+		faunaSpecies.setGhMax(1);
+		faunaSpecies.setKhMin(10);
+		faunaSpecies.setKhMax(1);
+		faunaSpecies.setTemperatureMin(10);
+		faunaSpecies.setTemperatureMax(1);
+
+		Set<ConstraintViolation<FaunaSpecies>> violations = validator.validate(faunaSpecies);
+
+		Assert.assertNotNull(violations);
+		Assert.assertEquals(4, violations.size());
+	}
+
+}
