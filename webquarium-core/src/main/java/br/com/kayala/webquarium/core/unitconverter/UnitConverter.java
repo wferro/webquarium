1970b180f3967605835d1f88351da13e3c102f10
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:51:17 -0200

    package name refactor

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/unitconverter/UnitConverter.java
@@ -0,0 +1,28 @@
+package br.com.kayala.webquarium.core.unitconverter;
+
+import java.math.BigDecimal;
+
+/**
+ * Unit conversion calculator interface
+ *
+ * @author kayala
+ */
+public interface UnitConverter {
+
+	/**
+	 * Converted from application raw value (liters) to desired unit
+	 *
+	 * @param rawValue raw value in liters
+	 * @return calculated unit value
+	 */
+	public BigDecimal convertedValue(BigDecimal rawValue);
+
+	/**
+	 * Calculate from unit to application raw value (liters)
+	 *
+	 * @param convertedValue calculated value to convert to raw
+	 * @return raw value in liters
+	 */
+	public BigDecimal rawValue(BigDecimal convertedValue);
+
+}
