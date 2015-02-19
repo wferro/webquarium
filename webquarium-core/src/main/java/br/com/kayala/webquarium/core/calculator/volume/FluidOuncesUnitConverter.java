1b8ba0d6a778305c47972b86faaaf26ed336345d
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:12:44 -0200

    Fluid ounces unit converter

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/calculator/volume/FluidOuncesUnitConverter.java
@@ -0,0 +1,24 @@
+package br.com.kayala.webquarium.core.calculator.volume;
+
+import java.math.BigDecimal;
+
+/**
+ * Fluid ounces unit converter
+ *
+ * @author kayala
+ */
+public class FluidOuncesUnitConverter implements VolumeCalculator {
+
+	private static final BigDecimal CONVERSION_CONSTANT = BigDecimal.valueOf(33.814D);
+
+	@Override
+	public BigDecimal convertedValue(BigDecimal rawValue) {
+		return rawValue.multiply(CONVERSION_CONSTANT);
+	}
+
+	@Override
+	public BigDecimal rawValue(BigDecimal convertedValue) {
+		return convertedValue.divide(CONVERSION_CONSTANT);
+	}
+
+}
