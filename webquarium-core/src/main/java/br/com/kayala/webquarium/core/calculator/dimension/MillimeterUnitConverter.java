f4d29ed1a40c246e38aea33f0b53ed293a020e39
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:10:47 -0200

    Millimeter unit converter

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/calculator/dimension/MillimeterUnitConverter.java
@@ -0,0 +1,23 @@
+package br.com.kayala.webquarium.core.calculator.dimension;
+
+import java.math.BigDecimal;
+
+/**
+ * Millimeter unit converter
+ * @author kayala
+ */
+public class MillimeterUnitConverter implements DimensionUnitConverter {
+
+	private static final BigDecimal CONVERSION_CONSTANT = BigDecimal.valueOf(10D);
+
+	@Override
+	public BigDecimal convertedValue(BigDecimal rawValue) {
+		return rawValue.multiply(CONVERSION_CONSTANT);
+	}
+
+	@Override
+	public BigDecimal rawValue(BigDecimal convertedValue) {
+		return convertedValue.divide(CONVERSION_CONSTANT);
+	}
+
+}
