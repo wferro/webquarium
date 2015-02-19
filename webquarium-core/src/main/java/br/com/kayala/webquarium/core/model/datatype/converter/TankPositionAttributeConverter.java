32b032177b4a53eccf71df1afb03d5f2269ff22d
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:43:21 -0200

    tank position enum jpa datatype attribute converter

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/model/datatype/converter/TankPositionAttributeConverter.java
@@ -0,0 +1,34 @@
+package br.com.kayala.webquarium.core.model.datatype.converter;
+
+import br.com.kayala.webquarium.core.model.datatype.TankPosition;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * tank position enum jpa datatype attribute converter
+ *
+ * @author kayala
+ */
+@Converter(autoApply = true)
+public class TankPositionAttributeConverter implements AttributeConverter<TankPosition, String> {
+
+	@Override
+	public String convertToDatabaseColumn(TankPosition x) {
+		if (x == null) {
+			return null;
+		} else {
+			return x.getDatabaseFlag();
+		}
+	}
+
+	@Override
+	public TankPosition convertToEntityAttribute(String y) {
+		if (StringUtils.trimToNull(y) == null) {
+			return null;
+		} else {
+			return TankPosition.getEnumByFlag(y);
+		}
+	}
+
+}
