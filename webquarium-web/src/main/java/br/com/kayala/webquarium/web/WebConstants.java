fab6ea1eedcd1343ea8bc9952ee1699de9c7880b
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 09:02:49 -0200

    web project constants class

---
+++ webquarium-web/src/main/java/br/com/kayala/webquarium/web/WebConstants.java
@@ -0,0 +1,13 @@
+package br.com.kayala.webquarium.web;
+
+/**
+ *
+ * @author kayala
+ */
+public class WebConstants {
+
+	public static final String CONTROLLER_PACKAGE = "br.com.kayala.webquarium.web.controller";
+	
+	public static final String REST_PACKAGE = "br.com.kayala.webquarium.web.rest";
+	
+}
