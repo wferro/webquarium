0f86b27706ef3755c6974c116adbca60e3833594
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:54:53 -0200

    compare operator enum

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/validator/annotation/CompareOperator.java
@@ -0,0 +1,15 @@
+package br.com.kayala.webquarium.core.validator.annotation;
+
+/**
+ * compare operator enum
+ *
+ * @author kayala
+ */
+public enum CompareOperator {
+
+	LOWER_THAN,
+	LOWER_THAN_OR_EQUALS,
+	EQUALS,
+	GREATER_THAN_OR_EQUALS,
+	GREATER_THAN
+}
