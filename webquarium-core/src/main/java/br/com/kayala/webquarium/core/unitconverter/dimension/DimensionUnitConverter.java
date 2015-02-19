3e7cf2b0350a0e32314823c0d4b61a89fa025bc3
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:51:32 -0200

    package name refactor

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/unitconverter/dimension/DimensionUnitConverter.java
@@ -0,0 +1,12 @@
+package br.com.kayala.webquarium.core.unitconverter.dimension;
+
+import br.com.kayala.webquarium.core.unitconverter.UnitConverter;
+
+/**
+ * Dimension unit converter interface
+ *
+ * @author kayala
+ */
+public interface DimensionUnitConverter extends UnitConverter {
+
+}
