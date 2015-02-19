7a7a2d729369943eddc0ff2576a5905fe7222b09
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:37:25 -0200

    volume measure unit datatype

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/model/datatype/Volume.java
@@ -0,0 +1,102 @@
+package br.com.kayala.webquarium.core.model.datatype;
+
+import br.com.kayala.webquarium.core.calculator.volume.CubicCentimeterUnitConverter;
+import br.com.kayala.webquarium.core.calculator.volume.FluidOuncesUnitConverter;
+import br.com.kayala.webquarium.core.calculator.volume.GallonUnitConverter;
+import br.com.kayala.webquarium.core.calculator.volume.LiterUnitConverter;
+import br.com.kayala.webquarium.core.calculator.volume.MilliliterUnitConverter;
+import br.com.kayala.webquarium.core.calculator.volume.VolumeUnitConverter;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.Min;
+
+/**
+ * volume measure unit datatype
+ *
+ * @author kayala
+ */
+@Embeddable
+public class Volume implements Serializable {
+
+	@Min(1)
+	private BigDecimal rawValue;
+
+	/**
+	 * application base value. in liters
+	 *
+	 * @return
+	 */
+	public BigDecimal getRawValue() {
+		return rawValue;
+	}
+
+	/**
+	 *
+	 */
+	public Volume() {
+	}
+
+	/**
+	 *
+	 * @param rawValue raw value in liters
+	 */
+	public Volume(BigDecimal rawValue) {
+		this.rawValue = rawValue;
+	}
+
+	/**
+	 *
+	 * @param volumeUnit volume unit for converted value
+	 * @param convertedValue
+	 */
+	public Volume(VolumeUnit volumeUnit, BigDecimal convertedValue) {
+		this.rawValue = volumeUnit.getCalculator().rawValue(convertedValue);
+	}
+
+	/**
+	 * get converted value from raw value to the specified unit
+	 *
+	 * @param volumeUnit target volume unit
+	 * @return converted value
+	 */
+	public BigDecimal getConvertedValue(VolumeUnit volumeUnit) {
+		return volumeUnit.getCalculator().convertedValue(rawValue);
+	}
+
+	/**
+	 * set converted value of a specified unit
+	 *
+	 * @param volumeUnit target volume unit
+	 * @param convertedValue
+	 */
+	public void setCalculatedValue(VolumeUnit volumeUnit, BigDecimal convertedValue) {
+		this.rawValue = volumeUnit.getCalculator().rawValue(convertedValue);
+	}
+
+	public enum VolumeUnit {
+
+		FluidOunces(FluidOuncesUnitConverter.class),
+		Gallon(GallonUnitConverter.class),
+		Liter(LiterUnitConverter.class),
+		Milliliter(MilliliterUnitConverter.class),
+		CubicCentimeter(CubicCentimeterUnitConverter.class);
+
+		private VolumeUnitConverter volumeCalculator;
+
+		private VolumeUnit(Class volumeCalculator) {
+			try {
+				this.volumeCalculator = (VolumeUnitConverter) volumeCalculator.newInstance();
+			} catch (InstantiationException | IllegalAccessException ex) {
+				Logger.getLogger(Volume.class.getName()).log(Level.SEVERE, null, ex);
+				this.volumeCalculator = new LiterUnitConverter();
+			}
+		}
+
+		private VolumeUnitConverter getCalculator() {
+			return volumeCalculator;
+		}
+	}
+}
