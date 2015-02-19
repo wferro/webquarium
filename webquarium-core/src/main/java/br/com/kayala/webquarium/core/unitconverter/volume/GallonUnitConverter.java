bd21bcaaa5be9dc770b19d908773ebee6b1301ac
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:51:58 -0200

    package name refactor

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/unitconverter/volume/GallonUnitConverter.java
@@ -0,0 +1,24 @@
+package br.com.kayala.webquarium.core.unitconverter.volume;
+
+import java.math.BigDecimal;
+
+/**
+ * Gallon unit converter
+ *
+ * @author kayala
+ */
+public class GallonUnitConverter implements VolumeUnitConverter {
+
+	private static final BigDecimal CONVERSION_CONSTANT = BigDecimal.valueOf(0.26417D);
+
+	@Override
+	public BigDecimal convertedValue(BigDecimal rawValue) {
+		return rawValue.multiply(CONVERSION_CONSTANT);
+	}
+
+	@Override
+	public BigDecimal rawValue(BigDecimal convertedValue) {
+		return convertedValue.divide(CONVERSION_CONSTANT);
+	}
+
+}
