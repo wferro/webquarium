a25d527809d3199cff0b2e3d1cae475e8d721539
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:50:40 -0200

    fauna species repository

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/model/repository/FaunaSpeciesRepository.java
@@ -0,0 +1,13 @@
+package br.com.kayala.webquarium.core.model.repository;
+
+import br.com.kayala.webquarium.core.model.entity.FaunaSpecies;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * fauna species repository
+ *
+ * @author kayala
+ */
+public interface FaunaSpeciesRepository extends JpaRepository<FaunaSpecies, Long> {
+
+}
