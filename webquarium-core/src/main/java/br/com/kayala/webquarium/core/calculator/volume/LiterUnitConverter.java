ec980727a1e92e205a9b86356a19c6c0f296ed1a
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:14:07 -0200

    Liter unit converter

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/calculator/volume/LiterUnitConverter.java
@@ -0,0 +1,22 @@
+package br.com.kayala.webquarium.core.calculator.volume;
+
+import java.math.BigDecimal;
+
+/**
+ * Liter unit converter
+ *
+ * @author kayala
+ */
+public class LiterUnitConverter implements VolumeCalculator {
+
+	@Override
+	public BigDecimal convertedValue(BigDecimal rawValue) {
+		return rawValue;
+	}
+
+	@Override
+	public BigDecimal rawValue(BigDecimal convertedValue) {
+		return convertedValue;
+	}
+
+}
