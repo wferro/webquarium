54991ca5d4b5016731bd099d5fe2d2ff857b60fc
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:51:36 -0200

    package name refactor

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/unitconverter/dimension/FeetUnitConverter.java
@@ -0,0 +1,24 @@
+package br.com.kayala.webquarium.core.unitconverter.dimension;
+
+import java.math.BigDecimal;
+
+/**
+ * Feet unit converter
+ *
+ * @author kayala
+ */
+public class FeetUnitConverter implements DimensionUnitConverter {
+
+	private static final BigDecimal CONVERSION_CONSTANT = BigDecimal.valueOf(2.54D);
+
+	@Override
+	public BigDecimal convertedValue(BigDecimal rawValue) {
+		return rawValue.divide(CONVERSION_CONSTANT);
+	}
+
+	@Override
+	public BigDecimal rawValue(BigDecimal convertedValue) {
+		return convertedValue.multiply(CONVERSION_CONSTANT);
+	}
+
+}
