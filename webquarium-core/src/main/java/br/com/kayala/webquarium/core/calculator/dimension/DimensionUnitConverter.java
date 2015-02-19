7964b4d75856e2c3609f0ce9090835f53ed4e155
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-19 08:08:38 -0200

    Dimension unit converter interface

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/calculator/dimension/DimensionUnitConverter.java
@@ -0,0 +1,12 @@
+package br.com.kayala.webquarium.core.calculator.dimension;
+
+import br.com.kayala.webquarium.core.calculator.UnitConverter;
+
+/**
+ * Dimension unit converter interface
+ *
+ * @author kayala
+ */
+public interface DimensionUnitConverter extends UnitConverter {
+
+}
