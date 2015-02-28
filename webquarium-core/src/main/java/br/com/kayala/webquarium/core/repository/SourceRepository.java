a4fb13d103a946f8ce6e8fe7a59d1b1ce23ee617
Rodrigo Bernardo <rodrigo@kayala.com.br>
2015-02-27 21:43:04 -0300

    Source JpaRepository

---
+++ webquarium-core/src/main/java/br/com/kayala/webquarium/core/repository/SourceRepository.java
@@ -0,0 +1,12 @@
+package br.com.kayala.webquarium.core.repository;
+
+import br.com.kayala.webquarium.core.model.entity.Source;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author kayala
+ */
+public interface SourceRepository extends JpaRepository<Source, Long> {
+
+}
